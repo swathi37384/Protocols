683714f77a90695a59610b6804028dda6b60f1ea
swathi37384 <swathi37384@gmail.com>
2026-08-19 22:10:16 +0530

    Update write_operation.v

--- i2c_protocol/write_operation.v
+++ i2c_protocol/write_operation.v
@@ -37,14 +37,6 @@ reg scl_en;
 always@(posedge clk or posedge rst)begin
 if(rst)begin
 	state<=idle;
-	sda_out<=0;
-	scl_out<=0;
-	addr_reg<=8'd0;
-	bit_count<=3'd0;
-	scl_en<=1'b0;
-	busy<=1'b0;
-	done<=1'b0;
-	ack_error<=1'b0;
 	
 end
 else begin
@@ -83,107 +75,160 @@ always@(posedge clk or posedge rst)begin
             end
     end
 
-    //next state logic
-
-    
-state<=next_state;
+//next state logic
+always@(*)begin
+next_state=state;
 case(state)
 
+idle:begin
+if(s)
+next_state=start;
+end
+
+start:begin
+if(scl_fall)
+next_state=send_addr;
+end
+
+send_addr:begin
+if(scl_rise && bit_count==3'd0)
+next_state=ack_addr;
+end
+
+ack_addr:begin
+if(scl_rise)begin
+if(sda==1'b0)
+next_state=pointer_addr;
+else
+next_state=stop;
+end
+end
+
+pointer_addr:begin
+if(scl_rise && bit_count==3'd0)
+next_state=pointer_ack;
+end
+
+pointer_ack:begin
+if(scl_rise)begin
+if(sda==1'b0)
+next_state=send_data;
+else
+next_state=stop;
+end
+end
+
+send_data:begin
+if(scl_rise&&bit_count==3'd0)
+next_state=data_ack;
+end
+
+data_ack:begin
+if(scl_rise)
+next_state=stop;
+end
+
+stop:begin
+next_state=idle;
+end
+
+default:next_state=idle;
+endacase
+end
+
+
+
+//main control
+always@(posedge clk or posedge rst)begin
+if(rst)begin
+	sda_ouut<=1'b1;
+	scl_en<=1'b0;
+	add_reg<=8'd0;
+	pointer_reg<=8'd0;
+	data_reg<=8'd0;
+	bit_count<=3'd0;
+	busy<=1'b0;
+	done<=1'b0;
+	ack_error<=1'b0;
+end
+else begin
+done<=1'b0;
+case(state)
 idle: begin
 	scl_en<=1'b0;
 	sda_out<=1'b1;
-	scl_out<=1'b1;
-	busy<=1'b0;
-	done<=1'b0;
+	busy<=1'b0
 	if(s)begin
 	busy<=1'b1;
+        ack_error<=1'b0;
 	addr_reg<={slave_add,rw};
+	pointer_reg<=pointer_add;
+	data-reg<=tx_data;
 	bit_count<=3'd7;
-	next_state<=start;
 	end
 end
 
 start: begin
-	scl_out<=1'b1;
 	sda_out<=1'b0;
 	scl_en<=1'b1;
-	next_state<=send_addr;
 	
 end
 	
 send_addr:begin
-	if((scl_out==1'b0)&&(clk_div==8'd25))begin
+	if(scl_fall)begin
 	sda_out<=addr_reg[7];
 	end
-	if((scl_out==1'b1)&&(clk_div==8'd49))begin
-		if(bit_count==3'd0)begin
-		next_state<=ack_addr;
-		end
-		else begin
+	if(bit_count!=3'd0)begin
 		addr_reg<={addr_reg[6:0],1'b0};
 		bit_count<=bit_count-1'b1;
 		end
 	end
 end
 ack_addr:begin
-	if((scl_out == 1'b0) &&(clk_div == 8'd25)) begin
+	if(scl_fall) begin
 		sda_out<=1'b1;
 	end
-	if ((scl_out == 1'b1) &&(clk_div == 8'd49)) begin
-
+	if(scl_rise)begin
        	 if (sda == 1'b0) begin
 			bit_count<=3'd7;
-			 pointer_reg<=pointer_add;
-			next_state<=pointer_addr;
 	end
 	else begin
 		ack_error<=1'b1;
-		next_state<=stop;
 	end
 	end
 end
 pointer_addr:begin
-	if((scl_out==1'b0)&&(clk_div==8'd25))begin
+	if(scl_fall)begin
 	sda_out<=pointer_reg[7];
 	end
-	if((scl_out==1'b1)&&(clk_div==8'd49))begin
-		if(bit_count==3'd0)begin
-		next_state<=pointer_ack;
-		end
-		else begin
-			pointer_reg<={pointer_reg[6:0],1'b0};
+	if(bit_count!=3'd0)begin
+		pointer_reg<={pointer_reg[6:0],1'b0};
 		bit_count<=bit_count-1'b1;
 		end
 	end
 end
 
 pointer_ack:begin
-	if((scl_out == 1'b0) &&(clk_div == 8'd25)) begin
+	if(scl_fall) begin
 		sda_out<=1'b1;
 	end
-	if ((scl_out == 1'b1) &&(clk_div == 8'd49)) begin
+	if (scl_rise)begin
 
        	 if (sda == 1'b0) begin
-			pointer_reg<=pointer_add;
 			bit_count<=3'd7;
-			next_state<=send_data;
 	end
 	else begin
 		ack_error<=1'b1;
-		next_state<=stop;
+		
 	end
 	end
 end
 
 send_data:begin
-	if((scl_out==1'b0)&&(clk_div==8'd25))begin
+	if(scl_fall)begin
 	sda_out<=data_reg[7];
 	end
-	if((scl_out==1'b1)&&(clk_div==8'd49))begin
-		if(bit_count==3'd0)begin
-		next_state<=data_ack;
-		end
-		else begin
+	if(bit_count!=3'd0)begin
 		data_reg<={data_reg[6:0],1'b0};
 		bit_count<=bit_count-1'b1;
 		end
@@ -191,26 +236,19 @@ send_data:begin
 end		
 
 data_ack:begin
-	if((scl_out == 1'b0) &&(clk_div == 8'd25)) begin
+	if(scl_fall) begin
 		sda_out<=1'b1;
 	end
-	if ((scl_out == 1'b1) &&(clk_div == 8'd49)) begin
+	if (scl_rise) begin
 
-       	 if (sda == 1'b0) begin
-			next_state<=stop;
-	end
-	else begin
+       	 if (sda != 1'b0) begin
 		ack_error<=1'b1;
-		next_state<=stop;
 	end
 	end
 end
 
 stop:begin
-if ((scl_out == 1'b0) && (clk_div == 8'd25)) begin
-                        sda_out <= 1'b0;
-
-                    end
+sda_out<=1'b0;
                     if (scl_out == 1'b1) begin
 
                         scl_en  <= 1'b0;
@@ -218,13 +256,9 @@ if ((scl_out == 1'b0) && (clk_div == 8'd25)) begin
 			busy<=1'b0;
 			done<=1'b1;
 
-                        next_state <= idle;
-
                     end
 
                 end
-
-default:next_state<=idle;
 endcase
 end
 end
